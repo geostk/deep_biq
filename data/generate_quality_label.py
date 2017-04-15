b2ead82bf22e1a0b2c56706676bd2c9407adea70
andy <synckey@gmail.com>
2017-04-15 16:28:37 +0800

    add generate quality label

---
+++ data/generate_quality_label.py
@@ -0,0 +1,31 @@
+import os
+
+pwd = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_label(mos):
+    label = -1
+    if mos <= 20:
+        label = 0
+    elif mos <= 40:
+        label = 1
+    elif mos <= 60:
+        label = 2
+    elif mos <= 80:
+        label = 3
+    elif mos <= 100:
+        label = 4
+    return label
+
+
+def gen_file(dir_name, out_file='train.txt'):
+    with open(out_file, 'w') as f:
+        mos = 0
+        for f_name in os.listdir(dir_name):
+            mos = float(f_name.split('_')[1])
+            label = get_label(mos)
+            f.write(os.path.join(dir_name, f_name) + ' ' + str(label) + '\n')
+
+
+gen_file(os.path.join(pwd, 'rawdata/cropped_train'), 'quality_train.txt')
+gen_file(os.path.join(pwd, 'rawdata/cropped_validation'), 'quality_validation.txt')
