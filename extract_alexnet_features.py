0e223f2974de7ec2c5ec4e73424c228340dc7609
andy <synckey@gmail.com>
2017-04-16 10:55:02 +0800

    add generate quality labeld

--- extract_alexnet_features.py
+++ extract_alexnet_features.py
@@ -113,8 +113,8 @@ def extract(generator, plpath, liblinear_features_path):
 
 
 def main():
-    extract(train_generator, 'data/12_train.pl', 'data/12_train.features.txt')
-    extract(val_generator, 'data/12_validation.pl', 'data/12_valid.features.txt')
+    extract(train_generator, 'data/17_train.pl', 'data/17_train.features.txt')
+    extract(val_generator, 'data/17_validation.pl', 'data/17_valid.features.txt')
 
 
 if __name__ == '__main__':
