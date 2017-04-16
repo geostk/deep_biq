66bbafd306b0fd06c8eeb3917fdb1762636f5316
andy <synckey@gmail.com>
2017-04-16 09:13:44 +0800

    add generate quality labeld

--- liblinear_eval.py
+++ liblinear_eval.py
@@ -11,14 +11,17 @@ https://kratzert.github.io/2017/02/24/finetuning-alexnet-with-tensorflow.html
 Author: Frederik Kratzert 
 contact: f.kratzert(at)gmail.com
 """
-
+import os
 import cPickle as pickle
 import numpy as np
 import tensorflow as tf
+from scipy.stats import pearsonr
+from liblinearutil import load_model, predict
 from alexnet import AlexNet
 from datagenerator import ImageDataGenerator
+from image_processing import crop_a_image
 
-tf.app.flags.DEFINE_integer('batch_size', 128,
+tf.app.flags.DEFINE_integer('batch_size', 32,
                             """Number of images to process in a batch.""")
 tf.app.flags.DEFINE_integer('num_classes', 5,
                             """Number of images to process in a batch.""")
@@ -29,8 +32,6 @@ tf.app.flags.DEFINE_string('extracted_validation_features_path', 'data/extracted
 FLAGS = tf.app.flags.FLAGS
 
 # Path to the textfiles for the trainings and validation set
-train_file = 'data/quality_train.txt'
-val_file = 'data/quality_validation.txt'
 
 # Learning params
 learning_rate = 0.001
@@ -43,10 +44,6 @@ num_classes = FLAGS.num_classes
 # How often we want to write the tf.summary data to disk
 
 # Path for tf.summary.FileWriter and to store model checkpoints
-filewriter_path = "quality_training"
-checkpoint_path = "alexnet_model/model_epoch12.ckpt-0"
-
-# Create parent path if it doesn't exist
 
 # TF placeholder for graph input and output
 x = tf.placeholder(tf.float32, [batch_size, 227, 227, 3])
@@ -65,56 +62,36 @@ features_op = model.fc7
 saver = tf.train.Saver()
 
 # Initalize the data generator seperately for the training and validation set
-train_generator = ImageDataGenerator(train_file,
-                                     horizontal_flip=True, shuffle=True, nb_classes=num_classes)
-val_generator = ImageDataGenerator(val_file, shuffle=False, nb_classes=num_classes)
-
-# Get the number of training/validation steps per epoch
-train_batches_per_epoch = np.floor(train_generator.data_size / batch_size).astype(np.int16)
-val_batches_per_epoch = np.floor(val_generator.data_size / batch_size).astype(np.int16)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 # saver.restore(sess, checkpoint_path)
 model.load_initial_weights(sess)
 
-
-def export_to_liblinear(x_vals, y_vals, filename):
-    print("generating liblinear features...")
-    with open(filename, 'w') as f:
-        for i, label in enumerate(y_vals):
-            features = x_vals[i]
-            line = str(label) + "\t"
-            for k, v in enumerate(features):
-                line = line + str(k + 1) + ":" + str(v) + " "
-            line = line.strip()
-            f.write(line + '\n')
-    f.close()
-    print("liblinear features done.")
+validation_dir = 'data/rawdata/validation'
+m = load_model("./liblinear.model")
+labels = []
+preds_min = []
+preds_avg = []
+preds_max = []
 
 
-def extract(generator, plpath, liblinear_features_path):
-    y_vals = np.array([])
-    x_vals = np.ndarray(shape=[0, 4096])
-    steps = len(generator.images) / batch_size
-    for m in range(steps):
-        batch_tx, scores, paths = train_generator.next_batch_with_filename(batch_size)
-        print (m, scores, paths)
+def evaluate():
+    for f_name in [os.path.join(validation_dir, f) for f in os.listdir(validation_dir)]:
+        batch_tx = crop_a_image(f_name, 227, 227, FLAGS.batch_size)
+        mos = float(f_name.split('_')[1].replace('.jpg', ''))
         features = sess.run(features_op, feed_dict={x: batch_tx, keep_prob: 1.})
-        x_vals = np.append(x_vals, features, axis=0)
-        y_vals = np.append(y_vals, scores)
-        if (m + 1) % 100 == 0 or m == (steps - 1):
-            with open(plpath, 'w') as  f:
-                features_map = {}
-                features_map['x'] = x_vals
-                features_map['y'] = y_vals
-                pickle.dump(features_map, f)
-            export_to_liblinear(x_vals, y_vals, liblinear_features_path)
-
+        pred_score = predict([], features, m, options="")[0]
+        preds_min.append(np.min(pred_score))
+        preds_max.append(np.max(pred_score))
+        preds_avg.append(np.average(pred_score))
+        print (mos, np.average(pred_score), np.max(pred_score), np.min(pred_score))
+        labels.append(mos)
+        print("avg_lcc", pearsonr(labels, preds_avg)[0], "min_lcc", pearsonr(labels, preds_min)[0], "max_lcc",
+              pearsonr(labels, preds_max)[0])
 
 
 def main():
-    extract(train_generator, 'data/12_train.pl', 'data/12_train.features.txt')
-    extract(val_generator, 'data/12_validation.pl', 'data/12_valid.features.txt')
+    evaluate()
 
 
 if __name__ == '__main__':
