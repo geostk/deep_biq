834a744f9d3162b9d8ae145f6c043adf131ea343
andy <synckey@gmail.com>
2017-04-17 15:25:51 +0800

    add generate quality labeld

--- svr.py
+++ svr.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import SGDRegressor
 from sklearn.svm import SVR, LinearSVR
 
 pls_dir = 'data/features/train/'
+validaiont_dir = 'data/features/validation/'
 y_vals = np.array([])
 x_vals = np.ndarray(shape=[0, 4096])
 svr_lin = SVR(kernel='rbf', C=1e3)
@@ -19,6 +20,7 @@ for file_name in os.listdir(pls_dir):
         x_vals = np.append(x_vals, x, axis=0)
         y_vals = np.append(y_vals, y)
         print (file_name, "loaded")
+
 shuffle_indexes = np.random.choice(len(y_vals), len(y_vals), replace=False)
 y_vals_train = y_vals[shuffle_indexes]
 x_vals_train = x_vals[shuffle_indexes]
@@ -27,13 +29,25 @@ print x_vals_train.shape
 # linear_svr = LinearSVR(C=1e3)
 linear_svr = SGDRegressor()
 linear_svr.fit(x_vals_train, y_vals_train)
-score_vals = np.array([])
+
+avg_score_vals = np.array([])
+min_score_vals = np.array([])
+max_score_vals = np.array([])
 label_vals = np.array([])
-for i in range(1000):
-    rand_index = np.random.choice(len(x_vals_train), size=128)
-    y_train = y_vals_train[rand_index]
-    x_train = x_vals_train[rand_index]
-    y = linear_svr.predict(x_train)
-    score_vals = np.append(score_vals, y)
-    label_vals = np.append(label_vals, y_train)
-    print pearsonr(score_vals, label_vals)[0]
+
+for file_name in os.listdir(validaiont_dir):
+    if not file_name.endswith('.pl'):
+        continue
+    with open(os.path.join(pls_dir, file_name)) as f:
+        f_map = pickle.load(f)
+        y = f_map.get("y")
+        x = f_map.get("x")
+        scores = linear_svr.predict(x)
+        print (file_name, "loaded")
+
+        avg_score_vals = np.append(avg_score_vals, np.average(scores))
+        min_score_vals = np.append(min_score_vals, np.min(scores))
+        max_score_vals = np.append(min_score_vals, np.max(scores))
+        label_vals = np.append(label_vals, y[0])
+        print (y[0], np.average(scores), np.min(scores), np.max(scores))
+        print(pearsonr(y, avg_score_vals)[0], pearsonr(y, min_score_vals)[0], pearsonr(y, max_score_vals)[0])
